dc68a5974ec702f915ffcd75171ffde37bf16624
JangKroed <didlsdydgh@gmail.com>
2022-11-14 18:38:13 +0900

    test

--- src/page.routes.js
+++ src/page.routes.js
@@ -5,10 +5,4 @@ const router = (0, express_1.Router)();
 router.get('/', (req, res) => {
     res.status(200).render('home.html');
 });
-router.get('/front', (_, res) => {
-    res.render('front.html');
-});
-router.get('/main', (req, res) => {
-    res.render('main.html');
-});
 exports.default = router;
